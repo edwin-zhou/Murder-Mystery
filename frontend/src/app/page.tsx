b6bc5e228f9fe2f4b5d39ac21b49fc6116f36e28
jiaqingyan <qingyan.jia@mail.mcgill.ca>
2025-01-25 22:35:08 -0500

    add flip cards and a table

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -91,6 +91,8 @@ export default function Home() {
               <FlipCard></FlipCard>
               <FlipCard></FlipCard>
               <FlipCard></FlipCard>
+              <FlipCard></FlipCard>
+              <FlipCard></FlipCard>
           </div>
 
 
