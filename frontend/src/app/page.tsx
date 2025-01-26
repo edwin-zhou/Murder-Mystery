8014d2c43988b1395544c5fffe296596e8840951
Edwin <edwin99@live.ca>
2025-01-26 11:58:29 -0500

    fix layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -88,68 +88,70 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          {/*<NeoVisGraph></NeoVisGraph>*/}
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Input type="file" accept="video/*" onChange={handleFileChange} />
-          <Button
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            onClick={handleUpload}
-          >
-            Upload Evidence Footage
-          </Button>
-        </div>
-          <div className="flip-container">
-          </div>
-          <Card>
-              <CardHeader>
-                  <CardTitle>Suspect Analysis</CardTitle>
-                  <CardDescription>Broken Down By Scene</CardDescription>
-              </CardHeader>
-              <CardContent>
-                  <div className="response-container">
-                      {response.length > 0 && (
-                          <ul>
-                              {response.map((item, index) => (
-                                  <li key={index}>
-                                    {item}
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+            {/*<NeoVisGraph></NeoVisGraph>*/}
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                SherlockAI: Crime Analysis
+            </h1>
+            <div className="flex gap-4 items-center flex-col sm:flex-row">
+                <Input type="file" accept="video/*" onChange={handleFileChange}/>
+                <Button
+                    className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+                    onClick={handleUpload}
+                >
+                    Upload Evidence Footage
+                </Button>
+            </div>
+            <div className="flip-container">
+            </div>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Suspect Analysis</CardTitle>
+                    <CardDescription>Broken Down By Scene</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div className="response-container">
+                        {response.length > 0 && (
+                            <ul>
+                                {response.map((item, index) => (
+                                    <li key={index} style={{ marginBottom: "10px" }}>
+                                        Scene {index + 1}: {item}
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
 
-                                  </li>
-                              ))}
-                          </ul>
-                      )}
-                  </div>
-
-                  {/*<ChartContainer config={chartConfig}>*/}
-                  {/*    <BarChart*/}
-                  {/*        accessibilityLayer*/}
-                  {/*        data={chartData}*/}
-                  {/*        layout="vertical"*/}
-                  {/*        margin={{*/}
-                  {/*            left: 0,*/}
-                  {/*        }}*/}
-                  {/*    >*/}
-                  {/*        <YAxis*/}
-                  {/*            dataKey="browser"*/}
-                  {/*            type="category"*/}
-                  {/*            tickLine={false}*/}
-                  {/*            tickMargin={10}*/}
-                  {/*            axisLine={false}*/}
-                  {/*            tickFormatter={(value) => chartConfig[value as keyof typeof chartConfig]?.label}*/}
-                  {/*        />*/}
-                  {/*        <XAxis dataKey="visitors" type="number" hide />*/}
-                  {/*        <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />*/}
-                  {/*        <Bar dataKey="visitors" layout="vertical" radius={5} />*/}
-                  {/*    </BarChart>*/}
-                  {/*</ChartContainer>*/}
-              </CardContent>
-              <CardFooter className="flex-col items-start gap-2 text-sm">
-              </CardFooter>
-          </Card>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    {/*<ChartContainer config={chartConfig}>*/}
+                    {/*    <BarChart*/}
+                    {/*        accessibilityLayer*/}
+                    {/*        data={chartData}*/}
+                    {/*        layout="vertical"*/}
+                    {/*        margin={{*/}
+                    {/*            left: 0,*/}
+                    {/*        }}*/}
+                    {/*    >*/}
+                    {/*        <YAxis*/}
+                    {/*            dataKey="browser"*/}
+                    {/*            type="category"*/}
+                    {/*            tickLine={false}*/}
+                    {/*            tickMargin={10}*/}
+                    {/*            axisLine={false}*/}
+                    {/*            tickFormatter={(value) => chartConfig[value as keyof typeof chartConfig]?.label}*/}
+                    {/*        />*/}
+                    {/*        <XAxis dataKey="visitors" type="number" hide />*/}
+                    {/*        <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />*/}
+                    {/*        <Bar dataKey="visitors" layout="vertical" radius={5} />*/}
+                    {/*    </BarChart>*/}
+                    {/*</ChartContainer>*/}
+                </CardContent>
+                <CardFooter className="flex-col items-start gap-2 text-sm">
+                </CardFooter>
+            </Card>
+        </main>
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+            <a
+                className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
